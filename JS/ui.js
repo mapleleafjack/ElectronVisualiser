273edf98027ea8fc0c06f84b749ada5f43310c53
mapleleafjack <mapleleafjack@gmail.com>
2020-05-22 14:07:49 +0100

    web fixs

--- JS/ui.js
+++ JS/ui.js
@@ -1,4 +1,6 @@
-$('#song').change(function() {
-  console.log('set ' + document.getElementById("song").files[0].path);
-    $('#audiofile').attr("src", document.getElementById("song").files[0].path);
-});
+var songload = document.getElementById('song');
+
+songload.onchange = function() {
+  var audiofile = document.getElementById('audiofile');
+  audiofile.src = songload.files[0].path;
+}
